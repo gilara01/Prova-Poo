564fe8d03011af46fea80e7c96d82eb05bccedc3
Giovanna <37631828+gilara01@users.noreply.github.com>
2023-11-13 16:37:33 -0300

    Create EX3-PROVA2-POO.cs

---
+++ EX3-PROVA2-POO.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prova2ex3
+{
+    internal class pilhapeça
+    {
+
+        string[] produto;
+        string[] peçasseparadas;
+
+        public pilhapeça(string[] produto)
+        {
+            this.produto = produto;
+        }
+
+        public void Trocar(string peçavelha, string peçanova)
+        {
+            while(produto.Length > 0) 
+            {
+                if (produto[0] == peçavelha)
+                {
+                    produto[0] = peçanova;
+                    Console.WriteLine("trocando" + produto[0]);
+
+                    for(int i = peçasseparadas.Length ; peçasseparadas.Length > 0; i--)
+                    {
+                        produto.Append(peçasseparadas[i]);
+                        Console.WriteLine("voltando" + produto[i]);
+
+                        List<string> list = new List<string>(peçasseparadas);
+                        list.RemoveAt(i);
+                        peçasseparadas = list.ToArray();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("retirando" + produto[0]);
+                    peçasseparadas.Append(produto[0]);
+                    List<string> list = new List<string>(produto);
+                    list.RemoveAt(0);
+                    produto = list.ToArray();
+                }
+            }
+        }
+    }
+}
